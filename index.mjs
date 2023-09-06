523e76a501a8ac8f0b5fab4e8fc8ea10c3dc2504
Kris477 <134212412+Kris477@users.noreply.github.com>
2023-09-06 17:33:32 +0000

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,19 @@
+console.log("Script started")
+import Client from "msgroom";
+import express from 'express';
+
+const app = express();
+const port = 443;
+
+app.get('/', (req, res) => {
+  res.send('Welcome! This is Kris47TaskBot server. Thank you for visiting! The bot will stay online longer');
+});
+app.listen(3000, '0.0.0.0');
+const client = new Client("Kris47TaskBot", [ "!" ]);
+await client.connect()
+client.commands.ping = () => "pong";
+client.commands.repeat = (reply, ...args) => {
+    return args.join(" ");
+};
+client.commands.update = () => "No updates found"
+client.commands.botinfo = () => "Node.js on Linux. Uses ECMAScript modules? Yes, it's actually the framework for MsgRoom bots. Remember: the server only has 0.6 CPU so don't abuse"
